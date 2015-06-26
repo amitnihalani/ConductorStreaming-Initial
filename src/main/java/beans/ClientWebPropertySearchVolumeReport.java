0858241f5efcec9f0ecd01d4c5014b241b0a728d
Amit Nihalani <ami.nihalani@gmail.com>
2015-06-26 15:10:28 -0400

    Search Volume Report Class

---
+++ src/main/java/beans/ClientWebPropertySearchVolumeReport.java
@@ -0,0 +1,74 @@
+package beans;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonPropertyOrder({ "averageVolume", "trackedSearchId", "volumeItems" })
+public class ClientWebPropertySearchVolumeReport {
+
+    private long averageVolume;
+    private long trackedSearchId;
+
+
+
+     private List<VolumeItem> volumeItems = new ArrayList<>();
+    //private Object volumeItems;
+
+    /**
+     *
+     * @return The averageVolume
+     */
+    @JsonProperty("averageVolume")
+    public long getAverageVolume() {
+        return averageVolume;
+    }
+
+    /**
+     *
+     * @param averageVolume
+     *            The averageVolume
+     */
+    public void setAverageVolume(long averageVolume) {
+        this.averageVolume = averageVolume;
+    }
+
+    /**
+     *
+     * @return The trackedSearchId
+     */
+    @JsonProperty("trackedSearchId")
+    public long getTrackedSearchId() {
+        return trackedSearchId;
+    }
+
+    /**
+     *
+     * @param trackedSearchId
+     *            The trackedSearchId
+     */
+    public void setTrackedSearchId(long trackedSearchId) {
+        this.trackedSearchId = trackedSearchId;
+    }
+
+    /**
+     *
+     * @return The volumeItems
+     */
+    @JsonProperty("volumeItems")
+    public List<VolumeItem> getVolumeItems() {
+        return volumeItems;
+    }
+
+    /**
+     *
+     * @param volumeItems
+     *            The volumeItems
+     */
+    public void setVolumeItems(List<VolumeItem> volumeItems) {
+        this.volumeItems = volumeItems;
+    }
+
+}
